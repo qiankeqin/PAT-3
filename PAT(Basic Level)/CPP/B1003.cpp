1c546b8c30a3fb9b816b7be7180f4a72e949eb5a
ultraji <ultraji@live.com>
2018-09-08 19:12:22 +0800

    2018.09.08

--- PAT(Basic Level)/CPP/B1003.cpp
+++ PAT(Basic Level)/CPP/B1003.cpp
@@ -1,8 +1,5 @@
 /*
 1003. 我要通过！(20)
-时间限制 400 ms
-内存限制 65536 kB
-代码长度限制 8000 B
 
 “答案正确”是自动判题系统给出的最令人欢喜的回复。本题属于PAT的“答案正确”大派送 —— 只
 要读入的字符串满足下列条件，系统就输出“答案正确”，否则输出“答案错误”。得到“答案正确”
@@ -44,43 +41,56 @@ NO
 */
 
 // 结论:
-// 只能出现P(只能出现一次),A,T(只能出现一次)三个字母,且必须都出现;不能出现其他字符
-// P之前的A的个数 * PT之间的A的个数 = T之后的A的个数
-// 推导：
-// 1: PAT			0 = 0
-// 2: xPATx			x = x
-// 3: 如果 aPbTc 要正确,根据1,2可得b只能为'A',即aPATc;再由2可得 a = c;
-//	  那么 aPbATca 即 aPAATac; 又 a=c => aPAATaa
+// 只能出现P(只能出现一次)，A，T(只能出现一次)三个字母，且必须都出现，不能出现其他字符；
+// P之前的A的个数 * PT之间的A的个数 = T之后的A的个数；
 
-#include <cstdio>
+#include <iostream>
 #include <cstring>
 
-int main(){
-    char str[101],tmp;
+#define MAX_SIZE 102
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    char str[MAX_SIZE], tmp;
     int n;
-    scanf("%d%c",&n,&tmp);
-    while(n--){
-        int p1 = 0,t1 = 0,a1 = 0,p2,t2,strl;//p1:P的个数; t1:T的个数; a1:A的个数; p2:P所在的位置; t2:T所在的位置
-        gets(str);
-        strl = strlen(str);
-        for(int i = 0; i < strl; i++){
-            if(str[i] == 'P'){
+    scanf("%d%c", &n, &tmp);
+    while(n--)
+    {
+        //p1:P的个数; t1:T的个数; a1:A的个数; p2:P所在的位置; t2:T所在的位置
+        int p1 = 0, t1 = 0, a1 = 0, p2, t2, len;
+        fgets(str, MAX_SIZE, stdin);
+        len = strlen(str) - 1;
+        for(int i = 0; i < len; i++)
+        {
+            if(str[i] == 'P')
+            {
                 p1 ++;
                 p2 = i;
-            }else if(str[i] == 'T'){
+            }else if(str[i] == 'T')
+            {
                 t1 ++;
                 t2 = i;
-            }else if(str[i] == 'A'){
+            }else if(str[i] == 'A')
+            {
                 a1 ++;
             }
         }
-        if(p1 != 1 || t1 != 1 || p1+t1+a1 < strl || t2 <= p2 + 1){ // P不只出现1次(0次) || T不只出现1次(0次)|| 有其他字符 || (P出现在T之前,PT相邻的情况)
-            printf("NO\n");
-        }else{
-            if(p2*(t2-p2-1) == strl-t2-1){	//P所在的位置index即P之前A的个数
-                printf("YES\n");
-            }else{
-                printf("NO\n");
+        // P不只出现1次(0次) || T不只出现1次(0次)|| 有其他字符 || (P出现在T之前,PT相邻的情况)
+        if(p1 != 1 || t1 != 1 || p1 + t1 + a1 < len || t2 <= p2 + 1)
+        { 
+            puts("NO");
+        }
+        else
+        {
+            //P所在的位置index即P之前A的个数
+            if(p2 * (t2 - p2 - 1) == len - t2 - 1 )    
+            {	
+                puts("YES");
+            }else
+            {
+                puts("NO");
             }
         }
     }
